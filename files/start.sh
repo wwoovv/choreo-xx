e72abfd14505c3ee6a14693c8c55048e1c4e4e6f
Okka <142894633+eoovve@users.noreply.github.com>
2023-10-27 19:36:47 +0800

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/bash
 export NEZHA_SERVER="nz.f4i.cn:5555"
+export NEZHA_PORT="5555"
+export NEZHA_TLS="1"
 export NEZHA_KEY="E47pcYnIJmnyhdEmA3"
 
+if [ "$NEZHA_TLS" -eq 0 ]; then
+  NEZHA_TLS=''
+elif [ "$NEZHA_TLS" -eq 1 ]; then
+  NEZHA_TLS='--tls'
+fi
+
 chmod +x swith web
-nohup ./swith -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &  # 若需要tls，则在本句${NEZHA_KEY}后面加上 --tls即可
+nohup ./swith -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS}> /dev/null 2>&1 &
 nohup ./web >/dev/null 2>&1 &
 
 tail -f /dev/null
