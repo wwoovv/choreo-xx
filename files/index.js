5889761788f693b292d44d3db2418ac65bf6bc5d
8818158 <142342666+8818158@users.noreply.github.com>
2023-10-29 04:53:44 +0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -1,8 +1,4 @@
-const express = require("express");
-const app = express();
 const { spawn } = require('child_process');
-const { createProxyMiddleware } = require("http-proxy-middleware");
-const port= process.env.PORT||8080;
 const shellFilePath = './start.sh';
 const childProcess = spawn('sh', [shellFilePath]);
 
@@ -18,9 +14,3 @@ childProcess.stderr.on('data', (data) => {
 childProcess.on('close', (code) => {
   console.log(`Child process exit, exit code：${code}`);
 });
-
-app.get("/", function(req, res) {
-  res.send("Hello world");
-});
-
-app.listen(port, () => console.log(`server is running on port:${port}！`));
