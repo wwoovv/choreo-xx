76d54b27315019cfa440d832c1226ac11634d2e7
Okko <142894633+eoovve@users.noreply.github.com>
2023-10-29 04:14:13 +0800

    commit

--- files/start.sh
+++ files/start.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/bash
+export UUID="c2a713bc-122c-4603-b516-87da2d6712f4"
 export NEZHA_SERVER="nz.f4i.cn"
 export NEZHA_PORT="5555"
 export NEZHA_TLS=""
 export NEZHA_KEY="E47pcYnIJmnyhdEmA3"
+export SERVER_PORT="3000"
 
 if [ "$NEZHA_TLS" -eq 0 ]; then
-  NEZHA_TLS=''
+      TLS=''
 elif [ "$NEZHA_TLS" -eq 1 ]; then
-  NEZHA_TLS='--tls'
+      TLS='--tls'
 fi
 
-chmod +x swith web
-nohup ./swith -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS}> /dev/null 2>&1 &
-nohup ./web >/dev/null 2>&1 &
+chmod +x swith server
+nohup ./swith -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${TLS}> /dev/null 2>&1 &
+nohup ./server -p ${SERVER_PORT} -u ${UUID} > /dev/null 2>&1 &
 
 tail -f /dev/null
